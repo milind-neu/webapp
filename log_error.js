813f92e771777c14a0aea76ba513df8a38066fd6
Milind Sharma <sharma.mil@northeastern.edu>
2023-03-30 19:12:28 -0400

    Changed node version

--- log_error.js
+++ log_error.js
@@ -2,8 +2,9 @@ const logger = require('./logger.js');
 
 const setAndLogError = (statusCode, errorMessage, response) => {
     logger.error(`${statusCode}: ${errorMessage}`);
-    response.status(statusCode);
-    response.json(errorMessage);
+    response.status(statusCode).json({
+        message: errorMessage
+    });
     return response
 }
 
--- packer/setup_cloudwatch.sh
+++ packer/setup_cloudwatch.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo yum update -y
+# sudo yum update -y
 
 # Download the CloudWatch Agent
 sudo wget https://s3.us-east-1.amazonaws.com/amazoncloudwatch-agent-us-east-1/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
