017b6bde193cb1afad2d3c233c6474ee24e0b149
Milind Sharma <sharma.mil@northeastern.edu>
2023-03-30 22:58:23 -0400

    Changed cloudwatch config

--- packer/setup_cloudwatch.sh
+++ packer/setup_cloudwatch.sh
@@ -6,3 +6,9 @@ sudo wget https://s3.us-east-1.amazonaws.com/amazoncloudwatch-agent-us-east-1/am
 
 # Install the package
 sudo rpm -U ./amazon-cloudwatch-agent.rpm
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+        -a fetch-config \
+        -m ec2 \
+        -c file:/opt/cloudwatch_config.json \
+        -s
